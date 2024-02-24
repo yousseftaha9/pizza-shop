302d4d82c41fc588a652ad1154435db0dc20b266
Youssef <youssefmtaha9999@gmail.com>
2024-02-24 21:00:57 +0200

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,9 +9,10 @@ export default defineConfig({
     eslint({
       overrideConfig: {
         rules: {
-          "no-unused-vars": "warn",
+          'no-unused-vars': 'warn',
         },
       },
     }),
   ],
+  base: '/pizza-shop/',
 });
